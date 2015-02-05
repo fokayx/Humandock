228cd09a0a72b5c292fa3914362ea56dd2dfccbe
fokayx <fokayx@gmail.com>
2015-02-05 14:41:33 +0800

    db create test

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,21 @@
 Rails.application.routes.draw do
+  get 'exams/index'
+
+  get 'exams/show'
+
+  get 'exams/new'
+
+  get 'exams/create'
+
+  resources :hospitals
+  resources :exams
+  resources :items
+  resources :warehouses
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-   root 'application#okay'
+   root 'hospitals#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
