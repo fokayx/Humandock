0cdaf029db148c37cdcf43ca21d3f5b5d0546b66
fokayx <fokayx@gmail.com>
2015-03-09 14:22:16 +0800

    search list ok

--- app/controllers/hospitals_controller.rb
+++ app/controllers/hospitals_controller.rb
@@ -1,6 +1,8 @@
 class HospitalsController < ApplicationController
 
   def index
+    @location = Location.find(params[:location])
     @hospitals = Hospital.where(location_id: params[:location])
+    @categories = Category.all
   end
 end
