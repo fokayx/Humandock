43fbe0206f9fcf2e2c098f428ceca23f9662101f
fokayx <fokayx@gmail.com>
2015-03-11 18:18:28 +0800

    examlist debug 3

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150309060627) do
+ActiveRecord::Schema.define(version: 20150311101448) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -67,14 +67,12 @@ ActiveRecord::Schema.define(version: 20150309060627) do
   end
 
   create_table "hospitals", force: :cascade do |t|
-    t.string   "name"
-    t.string   "address"
-    t.string   "area"
-    t.string   "phone"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-    t.string   "web"
-    t.integer  "location_id"
+    t.string  "name"
+    t.string  "address"
+    t.string  "area"
+    t.string  "phone"
+    t.string  "web"
+    t.integer "location_id"
   end
 
   create_table "items", force: :cascade do |t|
