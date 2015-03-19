88523e19fde5b367d2162168aedcd8698162f218
fokayx <fokayx@gmail.com>
2015-03-19 19:06:45 +0800

    h3

--- app/controllers/exams_controller.rb
+++ app/controllers/exams_controller.rb
@@ -1,12 +1,12 @@
 class ExamsController < ApplicationController
   def index
-   # @exams = Exam.where(hospital_id: params[:hospital])
-    @hospital = Hospital.first
-    #@hospital = Hospital.find(params[:hospital])
+    @exams = Exam.where(hospital_id: params[:hospital])
+   #@hospital = Hospital.first
+    @hospital = Hospital.find(params[:hospital])
   end
   def show
-    @exam = Exam.find(params[:id])
-    @examitem = @exam.items
+   #@exam = Exam.find(params[:id])
+   #@examitem = @exam.items
   end
 
   def new
